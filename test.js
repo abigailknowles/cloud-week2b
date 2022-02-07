3739a89a13f0e63537a61fa8bf3e1aebcb050ba5
abigailknowles <abigailknowlesx@yahoo.co.uk>
2022-02-07 11:23:24 +0000

    inital commit

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello Virtual Machine!')
+    res.send('Hello from the cluster!')
 })
 
 app.listen(port, () => {
